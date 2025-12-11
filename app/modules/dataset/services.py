84c9b08ffa7b4ffba7317970802f80b2448f7221
MartaAguilarMorcillo <marta.aguilar.morcillo1@gmail.com>
2025-12-11 01:35:28 +0100

    fix(lint): Arreglar fallo de formato usando black para que no falle el CI de Lint

--- app/modules/dataset/services.py
+++ app/modules/dataset/services.py
@@ -410,7 +410,7 @@ class DataSetService(BaseService):
     def update_dsmetadata(self, ds_id, **kwargs):
         return self.dsmetadata_repository.update(ds_id, **kwargs)
 
-#    MÉTODO MODIFICADO PARA SOLUCIONAR PROBLEMA URL PARA VISUALIZAR DATASETS DE PRUEBA EN DESPLIEGUE
+    #    MÉTODO MODIFICADO PARA SOLUCIONAR PROBLEMA URL PARA VISUALIZAR DATASETS DE PRUEBA EN DESPLIEGUE
 
     def get_csvhub_doi(self, dataset: DataSet) -> str:
         return f"/doi/{dataset.ds_meta_data.dataset_doi}/"
