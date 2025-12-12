3b93cf7cdd5d0d292a9409517e20127bafe26845
luisemmanuelchavezm <emmanuelchavezzzm@gmail.com>
2025-12-12 02:50:04 +0100

    chore(mejorarcodigo): se eliminaron elementos antiguos relacionados con featuremodel

--- app/modules/csvmodel/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template
-
-from app.modules.csvmodel import csvmodel_bp
-
-
-@csvmodel_bp.route("/csvmodel", methods=["GET"])
-def index():
-    return render_template("csvmodel/index.html")
